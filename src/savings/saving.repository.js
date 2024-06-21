d3a49b27ba9cae0b5581a96c8ea8d9eb85db7387
Rizqi Fajri <rizqifajri51@gmail.com>
2024-06-21 21:03:29 +0700

    feat: add collected fields for savings

--- src/savings/saving.repository.js
+++ src/savings/saving.repository.js
@@ -20,10 +20,10 @@ const findSavingsByUserId = async (userId)=> {
 }
 
 const insertSaving = async (dataSaving) => {
-  const {date, saving_method, total_saving, user_id, category_name, start_date, end_date, saving_frequency, nominal} = dataSaving;
-  const query = `INSERT INTO savings (date, saving_method, total_saving, user_id, category_name, start_date, end_date, saving_frequency, nominal)
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`
-  const values = [date, saving_method, total_saving, user_id, category_name, start_date, end_date, saving_frequency, nominal]
+  const {date, saving_method, total_saving, user_id, category_name, start_date, end_date, saving_frequency, nominal, collected} = dataSaving;
+  const query = `INSERT INTO savings (date, saving_method, total_saving, user_id, category_name, start_date, end_date, saving_frequency, nominal, collected)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`
+  const values = [date, saving_method, total_saving, user_id, category_name, start_date, end_date, saving_frequency, nominal, collected]
   const {rows} = await pool.query(query, values)
   return rows
 }
@@ -34,14 +34,14 @@ const deleteSaving = async (savingId) => {
 }
 
 const editSaving = async (dataSaving) => {
-  const {savingId, date, saving_method, total_saving, user_id, category_name, start_date, end_date, saving_frequency, nominal} = dataSaving;
+  const {savingId, date, saving_method, total_saving, user_id, category_name, start_date, end_date, saving_frequency, nominal, collected} = dataSaving;
   const query = `
   UPDATE savings 
   SET date = $1, saving_method = $2, total_saving = $3, user_id = $4, category_name = $5, start_date = $6, end_date = $7, 
-  saving_frequency = $8, nominal= $9
-  WHERE id = $10
+  saving_frequency = $8, nominal= $9, collected =$10
+  WHERE id = $11
   RETURNING *`
-  const values = [date, saving_method, total_saving, user_id, category_name, start_date, end_date, saving_frequency, savingId]
+  const values = [date, saving_method, total_saving, user_id, category_name, start_date, end_date, saving_frequency, nominal, collected, savingId]
   const {rows} = await pool.query(query, values)
   return rows
 }
